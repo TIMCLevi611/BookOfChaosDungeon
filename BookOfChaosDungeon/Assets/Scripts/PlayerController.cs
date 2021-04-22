563a3654ab20857c96ea4d07c1285883ec324466
Levi Snyder <19768@my4county.net>
2021-04-22 11:31:07 -0400

    Progress of 11:30 AM

--- BookOfChaosDungeon/Assets/Scripts/PlayerController.cs
+++ BookOfChaosDungeon/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject camra;
-    private float speed = 1f;
+    private float speed = 50f;
     private Vector3 camraOffset = new Vector3(0, 6, 0);
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         camra = GameObject.Find("Main Camera");
-        camra.transform.Translate(transform.position.x, transform.position.y + 6, transform.position.z);
+        camra.transform.Translate(transform.position + camraOffset);
     }
 
     // Update is called once per frame
@@ -29,15 +29,14 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * speed * verticalInput);
-        transform.Translate(Vector3.right * speed * horizontalInput);
+        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
     }
 
     public void MoveCamra()
     {
         if (transform.position.x != camra.transform.position.x || transform.position.z != camra.transform.position.z)
         {
-            //camra.transform.Translate(transform.position.x, camra.transform.position.y, transform.position.z);
             camra.transform.position = transform.position + camraOffset;
         }
     }
